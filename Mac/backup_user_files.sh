22b9988888c389a38e0c01a7fd9708f8650160f4
G. Reid <reid_garrett@yahoo.com>
2026-01-16 15:19:27 -0600

    Modified mac script a bit

--- Mac/backup_user_files.sh
+++ Mac/backup_user_files.sh
@@ -4,7 +4,7 @@
 
 USERNAME=$1
 USERFOLDER="/Users/$USERNAME"
-OUTPUT="/Volumes/Backup/${USERNAME}_backup_$(date +%Y%m%d).zip"
+OUTPUT="/Volumes/Backup/${USERNAME}_backup_$(date +%Y%m%d)"
 
 cd "$USERFOLDER" || exit
 zip -e -r "$OUTPUT" Documents Desktop Downloads
