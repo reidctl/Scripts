c837d17159e937eee25b0ce5bcbc451d284c3603
G. Reid <reid_garrett@yahoo.com>
2026-01-20 16:31:59 -0600

    Added section to chown user folder

--- Mac/backup_user_files.sh
+++ Mac/backup_user_files.sh
@@ -6,6 +6,11 @@ USERNAME=$1
 USERFOLDER="/Users/$USERNAME"
 OUTPUT="/Volumes/Backup/${USERNAME}_backup_$(date +%Y%m%d)"
 
+# Take ownership of user file recursively
+echo "Taking ownership of $USERNAME folder"
+chown -R tas "$USERFOLDER"
+
+
 cd "$USERFOLDER" || exit
 zip -e -r "$OUTPUT" Documents Desktop Downloads
 
